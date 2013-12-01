b961d8aa83b839362531bb46e32bbf373987cae4
Elliot Voris <elliotfriend@gmail.com>
2013-12-01 03:53:43 -0600

    Added some comments to this bad boy!
    Signed-off-by: Elliot Voris <elliotfriend@gmail.com>

--- YNAB4_LinuxInstall.pl
+++ YNAB4_LinuxInstall.pl
@@ -25,14 +25,18 @@ sub mydie {
   exit 1;
 }
 
+# Ensure that all dependencies are met (Base64 & WINE)
 eval "use MIME::Base64;";
 mydie "This script requires the Perl MIME::Base64 module to work, which you seem to be missing: $@\n" if $@;
 
 my $WINE = '/usr/bin/wine';
 mydie "\nYNAB 4 requires WINE to work, please install WINE and try again\n" unless -x $WINE;
 
+# Take a(n optional) argument to be a YNAB windows installer
 my $YNAB_WINDOWS = $ARGV[0];
 my $INSTALL_MODE = 'YNAB';
+
+# If an installer wasn't specified, ask the user what they want to do
 unless ($YNAB_WINDOWS && -s $YNAB_WINDOWS) {
   print <<"END_MESSAGE";
 Would you like to:
@@ -41,6 +45,7 @@ Would you like to:
 END_MESSAGE
   ;
   print "Select an option: [1] ";
+  # Take the user's response from STDIN, and we'll go from there
   my $ans = <STDIN>;
   my ($num) = ($ans =~ /(\d+)/);
   $num = 1 if $ans =~ /^\s*$/;
@@ -52,54 +57,70 @@ END_MESSAGE
   }
 }
 
+# If we're trying to install YNAB, but no installer has been specified
+# or the installer specified is just an empty file
 if ($INSTALL_MODE eq 'YNAB' && (!$YNAB_WINDOWS || !-s $YNAB_WINDOWS)) {
   print "\nSearching for YNAB4 Installer...\n";
+  # empty array
   my @installers;
 
+  # places to search for an installer
   my @search_paths = ('.',
                       $ENV{HOME} . "/Downloads",
                       '/tmp',
                       $ENV{HOME} . "/Dropbox",
                       $ENV{HOME},
                      );
+  # search through each of the paths for an installer
   foreach my $search_path (@search_paths) {
     print "Searching in $search_path\n";
     &find_installers($search_path, \@installers);
     last if @installers;
   }
   if (!@installers) {
+    # If no installers are found, quit
     mydie("Unable to find YNAB4 installer\n");
   }
   if (@installers == 1) {
+    # If one (1) installer is found, use that
     $YNAB_WINDOWS = $installers[0];
     print "\nFound Installer: '$installers[0]'\n\n";
   }
   else {
     $YNAB_WINDOWS = '';
     while (!$YNAB_WINDOWS) {
+      # If multiple installers are found, list them
       print "\nAvailable Installers:\n";
       @installers = reverse(@installers);
       for (my $i = 0; $i < @installers; $i++) {
         print "  " . $i+1 . ". " . $installers[$i] . "\n";
       }
       print "Select an installer: [1] ";
+      # Ask the user to select which installer to use
       my $ans = <STDIN>;
+      # check if the response is a digit
       my ($num) = ($ans =~ /(\d+)/);
+      # if the response is just whitespace, assume the default [1] (newest version found)
       $num = 1 if $ans =~ /^\s*$/;
       if ($num > 0 && $num <= @installers) {
+        # select the installer, provided it's a valid selection
         $YNAB_WINDOWS = $installers[$num-1];
       }
     }
     print "\n";
   }
 }
+
+# The user is trying to install YNAB, but something has gone wrong
 if ($INSTALL_MODE eq 'YNAB' && (!$YNAB_WINDOWS || !-s $YNAB_WINDOWS)) {
   mydie "\nNo YNAB4 Installer found!\n";
 }
 
+# Get started by opening the dropbox configuration
 my $DROPBOX_HOSTDB = $ENV{HOME} . "/.dropbox/host.db";
 my $DROPBOX_INSTALLDIR = "";
 if (-s $DROPBOX_HOSTDB) {
+  # Find and return the location of the Dropbox installation
   open(HOSTDB, $DROPBOX_HOSTDB) or mydie "Unable to read Dropbox configuration file";
   my $line1 = <HOSTDB>;
   my $b64_location = <HOSTDB>;
@@ -113,10 +134,12 @@ if (-s $DROPBOX_HOSTDB) {
 #$DROPBOX_INSTALLDIR = undef;
 if ($DROPBOX_INSTALLDIR) {
   if (! -d $DROPBOX_INSTALLDIR) {
+    # Dropbox setup hasn't been completed yet
     print "Dropbox detected but not found in '$DROPBOX_INSTALLDIR'\n";
     $DROPBOX_INSTALLDIR = '';
   }
   else {
+    # Dropbox was successfully found
     print "Found Dropbox Installation: '$DROPBOX_INSTALLDIR'\n";
   }
 }
@@ -163,6 +186,7 @@ END_MESSAGE
   }
 }
 
+# Suggest a winedir for YNAB, but ask for input from the user
 my $WINEDIR = $ENV{HOME} . "/.wine_YNAB4";
 print "\nSpecify WINE directory to use: [$WINEDIR] ";
 my $input = <STDIN>;
@@ -172,14 +196,16 @@ my $WINE_DRIVEC_DIR = "$WINEDIR/drive_c";
 my $WINE_APPDATA_DIR = "$WINE_DRIVEC_DIR/users/$ENV{USER}/Application\ Data";
 
 if ($INSTALL_MODE eq 'YNAB') {
+  # Create the winedir, unless it already exists
   # Might need to use $ENV{LOGNAME} here?
   system('mkdir', '-p', "$WINEDIR");
   mydie "Unable to create $WINEDIR\n" unless -d $WINEDIR;
 }
 else {
-  # Check to see if YHAB is installed already..
+  # Check to see if YNAB is installed already..
   my $YNAB_APPDATA_DIR = "$WINE_APPDATA_DIR/com.ynab.YNAB4.LiveCaptive";
   if (! -d $YNAB_APPDATA_DIR) {
+    # Something is here, but it doesn't look like YNAB. Better check with the user
     print "\nWARNING: YNAB4 does not appear to be installed in $WINEDIR\n";
     print "Continue Linking Dropbox? [yN] ";
     my $ans = <STDIN>;
@@ -189,6 +215,7 @@ else {
   }
 }
 
+# Twist WINE's arm to play nice with Dropbox
 if ($DROPBOX_INSTALLDIR) {
   print "\nConfiguring $WINEDIR for Dropbox\n";
   my $DROPBOX_WINE_CONFIG_DIR = "$WINE_APPDATA_DIR/Dropbox";
@@ -212,6 +239,7 @@ if ($DROPBOX_INSTALLDIR) {
   }
 }
 
+# Actually get down to installing YNAB, and keep track of everything in our log
 print "\nInstalling YNAB4 in $WINEDIR\n";
 my $INSTALL_LOG = '/tmp/ynab4_install.log';
 print "Installer output will be in $INSTALL_LOG\n";
@@ -242,26 +270,34 @@ unless ($ENV{_}) {
 }
 
 sub find_installers ($\@) {
+  # Take our two arguments and recursively search for a YNAB installer
   my ($dir, $found) = @_;
   &recursive_find_installers($dir, $found);
 }
 
 sub recursive_find_installers ($\@) {
+  # Snatch up our two arguments again
   my ($dir, $found) = @_;
 
+  # Open the directory we're currently looking in and create an array of filenames
   opendir(DIR, $dir) or return;
   my @files = readdir DIR;
   closedir DIR;
 
+  
   foreach my $file (sort @files) {
+    # Don't even think about those pesky hidden files
     next if $file =~ /^\./;
     my $path = "$dir/$file";
 
+    # Don't bother with symbolic links, either
     next if -l $path;
+    # If we've stumbled upon a directory, search through that, too
     if (-d $path) {
       &recursive_find_installers($path, $found);
     }
 
+    # If an installer exists, add it to the end of our @installers array
     if ($file =~ /^YNAB.*4.*setup.*\.exe$/i) {
       push @$found, $path;
     }
